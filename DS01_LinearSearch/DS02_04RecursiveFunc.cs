c53741087213677a483a2cef3b9d00725397c20c
woosangje <dnedrbs3@naver.com>
2022-01-01 16:28:06 +0900

    0101a

---
+++ DS01_LinearSearch/DS02_04RecursiveFunc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+namespace DS03_BSWorstOpCount
+{
+   
+    class DS02_04RecursiveFunc
+    {
+       static void Recursive(int num)
+        {
+            if (num <= 0)
+                return;
+            WriteLine("Recursive call! {0}", num);
+            Recursive(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            
+            Recursive(3);
+            
+
+
+        }
+    }
+   
+}
