4b2cea129530fd1b7c16264599add445f1398a21
woosangje <dnedrbs3@naver.com>
2022-01-19 11:43:36 +0900

    0119a

--- DS01_LinearSearch/DS03_01Point.cs
+++ DS01_LinearSearch/DS03_01Point.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 namespace DS03_01ListQuesion
 {
     struct _Point
@@ -8,28 +8,78 @@ namespace DS03_01ListQuesion
         public int xpos;
         public int ypos;
 
-        public _Point(int xpos, int ypos)
+        public void SetPointPos(int xpos, int ypos)
         {
             this.xpos = xpos;
             this.ypos = ypos;
         }
-        public override string ToString()
+        public void ShowPointPos()
         {
             Console.WriteLine("[{0}, {1}]", this.xpos, this.ypos);
         }
+
+        public int PointComp(_Point pos1, _Point pos2)
+        {
+            if (pos1.xpos == pos2.xpos && pos1.ypos == pos2.ypos)
+                return 0;
+            else if (pos1.xpos == pos2.xpos)
+                return 1;
+            else if (pos1.ypos == pos2.ypos)
+                return 2;
+            else
+                return -1;
+        }
+
+
     }
-    class Point
-    {
-      
-    }
+
+
+
+
     class MainApp
     {
 
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
-          
+            ArrayList list = new ArrayList();
+            _Point compPos = new _Point();
+            _Point ppos = new _Point();
 
+            ppos = new _Point();
+            ppos.SetPointPos(2, 1);
+            list.Add(ppos);
+
+            ppos = new _Point();
+            ppos.SetPointPos(2, 2);
+            list.Add(ppos);
+
+            ppos = new _Point();
+            ppos.SetPointPos(3, 1);
+            list.Add(ppos);
+
+            ppos = new _Point();
+            ppos.SetPointPos(3, 2);
+            list.Add(ppos);
+
+            Console.WriteLine("현재 데이터의 수: {0}", list.Count);
+
+            foreach (_Point i in list)
+            {
+                i.ShowPointPos();
+            }
+
+            //
+            compPos.xpos = 2;
+            compPos.ypos = 0;
+
+            for(int i=0; i<list.Count; i++)
+            {
+                ppos = list[i] as ppos;
+                if (list[i].PointComp(ppos, compPos) == 1)
+                {
+                    //ppos = i.RemoveAt();
+                }
+            }
 
         }
 
