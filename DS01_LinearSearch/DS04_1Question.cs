01756e9837f78e6620ff7f2805c09bcebfbde92c
woosangje <dnedrbs3@naver.com>
2022-01-29 23:12:20 +0900

    0129a

--- DS01_LinearSearch/DS04_1Question.cs
+++ DS01_LinearSearch/DS04_1Question.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 namespace DS03_02Question
 {
     //열혈106p
-    /*
+    
     public class _node
     {
         public int data;
 
-        public _node next;
+        public _node before;
     }
     class MainApp
     {
@@ -26,69 +26,49 @@ namespace DS03_02Question
 
             while (Convert.ToBoolean(1))
             {
-                Console.Write("자연수 입력 :");
+                Console.Write("자연수 입력 : ");
                 readData = int.Parse(ReadLine());
                 if (readData < 1)
                     break;
 
                 //노드의 추가과정
                 newNode = new _node();
-                newNode.data = readData;
-                newNode.next = null;
+                newNode.data = readData;//1
+                newNode.before = null;
 
-                if (head == null)
+                if (tail == null)//1 null  =  tail 1 2 null
                 {
-                    head = newNode;
-                    Write("테스트{0}", head.data);
+                    tail = newNode;
                 }
                 else
-                    tail.next = newNode;
+                    tail.before = newNode;
 
-                tail = newNode;
+                head = newNode;//1,2 null
             }
             Write("\n");
 
+            //입력 받은 데이터의 출력과정
             WriteLine("입력 받은 데이터의 전체출력!");
-            if(head ==null)
+            if (head == null)
             {
-                WriteLine("저장된 자연수가 존재하지 않습니다.");
+                WriteLine("저장된 자연수가 존재하지 않습니다. ");
             }
             else
             {
                 cur = head;
                 WriteLine(cur.data);
 
-                while(cur.next != null)
+                while (cur.next != null)
                 {
                     cur = cur.next;
                     WriteLine(cur.data);
                 }
             }
             WriteLine("\n");
-
-            if(head == null)
-            {
-
-            }
-            else
-            {
-                _node delNode = head;
-                _node delNextNode = head.next;
-
-                WriteLine("{0}을(를) 삭제합니다.", head.data);
-                delNode = null;
-                while(delNextNode !=null)
-                {
-                    delNode = delNextNode;
-                    delNextNode = delNextNode.next;
-
-                    WriteLine("{0}을(를) 삭제합니다.", delNode.data);
-                    delNode = null;
-                }
-            }
+            
         }
 
     }
-    */
+    
 
 }
