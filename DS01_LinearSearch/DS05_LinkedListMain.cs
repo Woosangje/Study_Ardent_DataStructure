7b8852c7a8f2de6d6e1e7b7185f51de9a703f631
woosangje <dnedrbs3@naver.com>
2022-02-22 22:03:44 +0900

    0222a

---
+++ DS01_LinearSearch/DS05_LinkedListMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.Collections;
+using System.Collections.Generic;
+using DS_LinkedList;
+namespace DS05_LinkedList {
+
+    class MainApp {
+
+        static void Main(string[] args) {
+            //원형 연결 리스트의 생성 및 초기화
+            CList list = new CList() ;
+            int data, nodeNum;
+
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.InsertFront(2);
+            list.InsertFront(1);
+
+            //리스트에 저장된 데이터를 연속 3회 출력
+            if (list.First(data)) {
+
+                WriteLine("{0} ", data);
+
+
+                
+
+            }
+            
+        }
+    }
+}
---
+++ DS01_LinearSearch/DS_LinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using static System.Console;
+using System.Collections;
+using System.Collections.Generic;
+namespace DS_LinkedList {
+    //열혈106p
+
+    class Node {
+        public int data;
+        public Node next;
+    }
+
+    class CList {
+        Node tail = null;
+        Node cur = null;
+        Node before = null;
+        int numOfData = 0;
+
+        public void InsertFront(int data) {
+
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (tail == null) {
+                tail = newNode;
+                newNode.next = newNode;
+            }
+            else {
+                newNode.next = tail.next;
+                tail.next = newNode;
+            }
+            numOfData++;
+        }
+
+        public void Insert(int data) {
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (tail == null) {
+                tail = newNode;
+                newNode.next = newNode;
+            }
+            else {
+                newNode.next = tail.next;
+                tail.next = newNode;
+                tail = newNode;//LInsertFront 함수와의 유일한 차이점
+            }
+            numOfData++;
+        }
+
+        public bool First(int data) {
+            if (tail == null)
+                return false;
+
+            before = tail;
+            cur = tail.next;
+
+            data = cur.data;
+            return true;
+        }
+        public bool Next(int data) {
+
+            if (tail == null)
+                return false;
+
+            before = cur;
+            cur = cur.next;
+
+            data = cur.data;
+            return true;
+
+        }
+
+        public int Remove( ) {
+            Node rpos = cur;
+            int rdata = rpos.data;
+
+            if (rpos == tail) {
+
+                if (tail == tail.next)
+                    tail = null;
+                else
+                    tail = before;
+            }
+
+            before.next = cur.next;
+            cur = before;
+
+            numOfData--;
+            return rdata;
+        }
+        public int Count() {
+            return numOfData;
+        }
+
+    }
+
+
+}
+
+       
+
+
+      
+    
+
+
+
